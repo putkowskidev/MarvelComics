fded8e2da520b8bdaa0af1951adfa09b7b793693
Putek-creator <lukii20012@gmail.com>
2023-02-15 21:33:12 +0100

    Initial Commit

---
+++ MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Łukasz  Putkowski on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
